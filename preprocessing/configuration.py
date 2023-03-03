1491e619b24560c9d727926e301aca82085b65e7
Tom Narock <thomas.narock@goucher.edu>
2023-03-03 13:10:48 -0500

    Created preprocessing folder

---
+++ preprocessing/configuration.py
@@ -0,0 +1,13 @@
+# UFOKN North Carolina geopackage files
+# These data were too large to include in GitHub
+# They are available at: https://zenodo.org/record/7696864#.ZAI3rXbMI2x
+dataDir = '/data/ufokn/building_classification/data/urmi2_37/' 
+
+# CENSUS NAICS county file
+naics = '/data/ufokn/building_classification/data/census_county_naics_2020.txt'
+
+# Output directory
+outDir = '/data/ufokn/building_classification/data/'
+
+# output file containing unique OSM key:value
+osmTypeFile = '/data/ufokn/building_classification/data/OSM_UniqueKeyValue.csv'
